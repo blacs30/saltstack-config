957639eba2e477162bfec4bfe3117537f3f8d022
blacs30 <github@lisowski-development.de>
2018-09-16 02:53:06 +0200

    Uncomment some zplug plugins. Don't need all at once. Add awslog to packages and update iterm window config.

--- states/zsh/includes/zplug.zsh
+++ states/zsh/includes/zplug.zsh
@@ -24,18 +24,18 @@ zplug "desyncr/auto-ls"
 zplug "plugins/git",                    from:oh-my-zsh
 zplug "plugins/aws",                    from:oh-my-zsh, if:"(( $+commands[aws] ))"
 zplug "plugins/history",               from:oh-my-zsh
-zplug "plugins/per-directory-history",  from:oh-my-zsh
+# zplug "plugins/per-directory-history",  from:oh-my-zsh
 zplug "junegunn/fzf", use:"shell/*.zsh", as:plugin, if:"(( $+commands[fzf] ))"
 zplug "plugins/terraform",                from:oh-my-zsh
 zplug "plugins/gitignore",                    from:oh-my-zsh
 zplug "plugins/python",                 from:oh-my-zsh
 zplug "plugins/dotenv",                 from:oh-my-zsh
-zplug "plugins/xcode",                  from:oh-my-zsh
+# zplug "plugins/xcode",                  from:oh-my-zsh
 zplug "plugins/brew",                  from:oh-my-zsh
-zplug "plugins/vagrant",                  from:oh-my-zsh
-zplug "plugins/vagrant-prompt",                  from:oh-my-zsh
+# zplug "plugins/vagrant",                  from:oh-my-zsh
+# zplug "plugins/vagrant-prompt",                  from:oh-my-zsh
 zplug "plugins/jira",                  from:oh-my-zsh
-zplug "plugins/sublime",                from:oh-my-zsh
+# zplug "plugins/sublime",                from:oh-my-zsh
 zplug "plugins/dash",                from:oh-my-zsh
 zplug "mafredri/zsh-async", from:"github", use:"async.zsh"
 zplug "willghatch/zsh-saneopt", from:"github", use:"saneopt", as:plugin
@@ -44,7 +44,7 @@ zplug "plugins/github",                    from:oh-my-zsh
 
 # disable as long as not really needed
 zplug "plugins/docker",                 from:oh-my-zsh
-zplug "plugins/docker-compose",         from:oh-my-zsh
+# zplug "plugins/docker-compose",         from:oh-my-zsh
 # zplug "plugins/kubectl",                from:oh-my-zsh
 # zplug "plugins/kube-ps1",                from:oh-my-zsh
 # zplug "plugins/minikube",                from:oh-my-zsh
@@ -56,8 +56,7 @@ zplug "plugins/docker-compose",         from:oh-my-zsh
 #zplug "themes/agnoster", from:oh-my-zsh, as:theme
 #zplug "eendroroy/alien"
 zplug "zakaziko99/agnosterzak-ohmyzsh-theme", use:agnosterzak.zsh-theme, as:theme, from:github
-
-zplug "powerline/fonts",                from:github, lazy:true
+# zplug "plugins/urltools",                from:oh-my-zsh
 
 zplug load
 
@@ -77,8 +76,8 @@ if [[ ! -f ~/.zshzplug-loaded ]]; then
 fi
 
 ## disabled plugins
+# zplug "powerline/fonts",                from:github, lazy:true
 # zplug "plugins/git-flow",                    from:oh-my-zsh, if:"$(git flow  > /dev/null 2>&1)"
-# zplug "plugins/urltools",                from:oh-my-zsh
 # zplug "plugins/sudo",                   from:oh-my-zsh
 # zplug "plugins/forklift",                from:oh-my-zsh
 # zplug "plugins/screen",                  from:oh-my-zsh
