fccea91141ea8f2c7544f5f025a5a457298c31ae
blacs30 <github@lisowski-development.com>
2018-12-03 01:36:56 +0100

    Add unetbootin and default for key-repeat for hyper terminal.

--- states/setup/files/macos-defaults.sh
+++ states/setup/files/macos-defaults.sh
@@ -100,6 +100,10 @@ defaults write com.apple.universalaccess closeViewZoomFollowsFocus -bool true
 # NOT - Disable press-and-hold for keys in favor of key repeat
 # defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool true
 
+# Disable press-and-hold for keys in favor of key repeat for hyper terminal
+defaults write co.zeit.hyper ApplePressAndHoldEnabled -bool
+false
+
 # NOT - Set a blazingly fast keyboard repeat rate
 #defaults write NSGlobalDomain KeyRepeat -int 1
 #defaults write NSGlobalDomain InitialKeyRepeat -int 10
